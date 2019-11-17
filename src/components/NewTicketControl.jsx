74facf1593c0367f6a1f53c9c30786e049caa1b0
Jacqueline Remmel <jpremmel@gmail.com>
2019-11-17 08:25:32 -0800

    Add routing and start working with state

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class NewTicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick(){
+    console.log('Hey, you clicked me!');
+  }
+
+  render(){
+    return(
+      <div>
+        <p>This is the NewTicketControl component.</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
+      </div>
+    );
+  }
+}
+
+export default NewTicketControl;
