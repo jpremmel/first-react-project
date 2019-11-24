56867c8557b8af0fb541ba97c8c64544e3604e13
Jacqueline Remmel <jpremmel@gmail.com>
2019-11-24 15:50:41 -0800

    Add ticket details for admin view; refactor extensively

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,42 +5,51 @@ import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 import Admin from './Admin';
 import { Switch, Route } from 'react-router-dom';
+import { v4 } from 'uuid';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      masterTicketList: []
+      masterTicketList: {},
+      selectedTicket: null
     };
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
   }
 
   handleAddingNewTicketToList(newTicket){
-    var newMasterTicketList = this.state.masterTicketList.slice();
-    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
-    newMasterTicketList.push(newTicket);
+    let newTicketId = v4();
+    let newMasterTicketList = Object.assign({}, this.state.masterTicketList, {
+      [newTicketId]: newTicket
+    }); //Object.assign() can create copies of objects and add new content to those copies in a single method call
+    newMasterTicketList[newTicketId].formattedWaitTime = newMasterTicketList[newTicketId].timeOpen.fromNow(true);
     this.setState({masterTicketList: newMasterTicketList});
   }
 
   updateTicketElapsedWaitTime() {
-    let newMasterTicketList = this.state.masterTicketList.slice();
-    newMasterTicketList.forEach((ticket) =>
-      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
-    );
+    let newMasterTicketList = Object.assign({}, this.state.masterTicketList);
+    Object.keys(newMasterTicketList).forEach(ticketId => {
+      newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true); //This invokes Moment to recalculate how much time has passed since the ticket was opened
+    });
     this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  handleChangingSelectedTicket(ticketId){
+    this.setState({selectedTicket: ticketId});
   }
 
   //Overriding component lifecycle methods - Look in the console to see when the React library is calling these as the program runs
   componentDidMount() {
-    console.log('componentDidMount');
+    //console.log('componentDidMount');
     this.waitTimeUpdateTimer = setInterval(() =>
       this.updateTicketElapsedWaitTime(),
     60000
     );
   }
   componentWillUnmount(){
-    console.log('componentWillUnmount');
+    //console.log('componentWillUnmount');
     clearInterval(this.waitTimeUpdateTimer);
   }
   // componentWillMount() {
@@ -71,7 +80,9 @@ class App extends React.Component {
             onNewTicketCreation={this.handleAddingNewTicketToList} />} />
           <Route path='/admin' render={(props)=><Admin 
             ticketList={this.state.masterTicketList} 
-            currentRouterPath={props.location.pathname} />} />
+            currentRouterPath={props.location.pathname}
+            onTicketSelection={this.handleChangingSelectedTicket}
+            selectedTicket={this.state.selectedTicket} />} />
           <Route component={Error404} />
         </Switch>
       </div>
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -6,22 +6,26 @@ function TicketList(props) {
   return (
     <div>
       <hr/>
-      {props.ticketList.map((ticket) =>
-        <Ticket 
+      {Object.keys(props.ticketList).map(function(ticketId) {
+        var ticket = props.ticketList[ticketId]; //this line isolates the ticket object corresponding to the current key
+        return <Ticket
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
           formattedWaitTime={ticket.formattedWaitTime}
           currentRouterPath={props.currentRouterPath}
-          key={ticket.id}/>
-      )}
+          key={ticketId}
+          ticketId={ticketId}
+          onTicketSelection={props.onTicketSelection} />;
+      })}
     </div>
   );
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array,
-  currentRouterPath: PropTypes.string
+  ticketList: PropTypes.object,
+  currentRouterPath: PropTypes.string,
+  onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
